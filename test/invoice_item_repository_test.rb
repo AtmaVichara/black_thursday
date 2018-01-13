9d3b20fc2735b3d3f24bc45fbaa60ab6622213bd
Joe Jobes <jmrjobes@gmail.com>
2018-01-13 14:03:26 -0700

    Starting refactoring of invoice_item_tests

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -7,7 +7,8 @@ class InvoiceItemRepositoryTest < Minitest::Test
   attr_reader :ir
 
   def setup
-    @ir = InvoiceItemRepository.new("./test/fixtures/invoice_items_sample.csv", "se")
+    parent = mock('sales_engine')
+    @ir = InvoiceItemRepository.new("./test/fixtures/invoice_items_sample.csv", parent)
   end
 
   def test_it_exists
@@ -15,17 +16,14 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_invoice_items_is_filled
-    assert ir.invoice_items.all? { |ii| ii.class == InvoiceItem }
     assert_equal 21, ir.invoice_items.count
-    refute_equal "14", ir.invoice_items.count
-  end
-
-  def test_all_returns_all_invoice_items
-    assert_equal 21, ir.all.count
-    assert ir.all.all? { |ii| ii.class == InvoiceItem}
+    ir.all.each do |invoice_item|
+      assert_instance_of InvoiceItem, invoice_item
+    end
   end
 
   def test_it_returns_invoice_item_by_id
+    skip
     invoice_item = ir.find_by_id(4)
 
     assert_instance_of InvoiceItem, invoice_item
@@ -36,6 +34,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_it_returns_all_items_by_id
+    skip
     invoice_items = ir.find_all_by_item_id(263454779)
 
     assert invoice_items.all? { |ii| ii.class == InvoiceItem }
@@ -44,6 +43,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_it_returns_all_invoice_items_by_invoice_id
+    skip
     invoice_items = ir.find_all_by_invoice_id(1)
 
     assert invoice_items.all? { |ii| ii.class == InvoiceItem }
